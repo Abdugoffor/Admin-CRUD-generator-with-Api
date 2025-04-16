dbd53399715148a33b9a6d2ab7b77bf1c1995bc3
Abdugoffor <abdugofforqodirov19@gmail.com>
2025-04-16 19:40:44 +0500

    fix

--- src/CrudGeneratorServiceProvider.php
+++ src/CrudGeneratorServiceProvider.php
@@ -9,7 +9,7 @@ class CrudGeneratorServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->commands([
-                Crud::class,
+                SimpleCrud::class,
                 ApiCrud::class
             ]);
         }
